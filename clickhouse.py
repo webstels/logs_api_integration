d30dd5cb2e05d01836304774097ac8b223161d8a
Andrey Kalmykov <webstels@yandex.ru>
2020-05-29 14:14:14 +0400

    Initial commit

---
+++ clickhouse.py
@@ -0,0 +1,183 @@
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
+import requests
+import urllib
+import urllib3
+import utils
+import sys
+import logging
+
+config = utils.get_config()
+CH_HOST = config['clickhouse']['host']
+CH_USER = config['clickhouse']['user']
+CH_PASSWORD = config['clickhouse']['password']
+CH_VISITS_TABLE = config['clickhouse']['visits_table']
+CH_HITS_TABLE = config['clickhouse']['hits_table']
+CH_DATABASE = config['clickhouse']['database']
+SSL_VERIFY = (config['disable_ssl_verification_for_clickhouse'] == 0)
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+logger = logging.getLogger('logs_api')
+
+
+def get_clickhouse_data(query, host=CH_HOST):
+    '''Returns ClickHouse response'''
+    logger.debug(query)
+    if (CH_USER == '') and (CH_PASSWORD == ''):
+        r = requests.post(host, data=query, verify=SSL_VERIFY)
+    else:
+        r = requests.post(host, data=query, auth=(CH_USER, CH_PASSWORD), verify=SSL_VERIFY)
+    if r.status_code == 200:
+        return r.text
+    else:
+        raise ValueError(r.text)
+
+
+def upload(table, content, host=CH_HOST):
+    '''Uploads data to table in ClickHous'''
+    content = content.encode('utf-8')
+    query_dict = {
+        'query': 'INSERT INTO ' + table + ' FORMAT TabSeparatedWithNames '
+    }
+    if (CH_USER == '') and (CH_PASSWORD == ''):
+        r = requests.post(host, data=content, params=query_dict, verify=SSL_VERIFY)
+    else:
+        r = requests.post(host, data=content, params=query_dict,
+                          auth=(CH_USER, CH_PASSWORD), verify=SSL_VERIFY)
+    result = r.text
+    if r.status_code == 200:
+        return result
+    else:
+        raise ValueError(r.text)
+
+
+def get_source_table_name(source, with_db=True):
+    '''Returns table name in database'''
+    if source == 'hits':
+        if with_db:
+            return '{db}.{table}'.format(db=CH_DATABASE, table=CH_HITS_TABLE)
+        else:
+            return CH_HITS_TABLE
+    if source == 'visits':
+        if with_db:
+            return '{db}.{table}'.format(db=CH_DATABASE, table=CH_VISITS_TABLE)
+        else:
+            return CH_VISITS_TABLE
+
+
+def get_tables():
+    '''Returns list of tables in database'''
+    return get_clickhouse_data('SHOW TABLES FROM {db}'.format(db=CH_DATABASE)) \
+        .strip().split('\n')
+
+
+def get_dbs():
+    ''''Returns list of databases'''
+    return get_clickhouse_data('SHOW DATABASES') \
+        .strip().split('\n')
+
+
+def is_table_present(source):
+    '''Returns whether table for data is already present in database'''
+    return get_source_table_name(source, with_db=False) in get_tables()
+
+
+def is_db_present():
+    '''Returns whether a database is already present in clickhouse'''
+    return CH_DATABASE in get_dbs()
+
+
+def create_db():
+    '''Creates database in clickhouse'''
+    return get_clickhouse_data('CREATE DATABASE {db}'.format(db=CH_DATABASE))
+
+
+def get_ch_field_name(field_name):
+    '''Converts Logs API parameter name to ClickHouse column name'''
+    prefixes = ['ym:s:', 'ym:pv:']
+    for prefix in prefixes:
+        field_name = field_name.replace(prefix, '')
+    return field_name[0].upper() + field_name[1:]
+
+
+def drop_table(source):
+    '''Drops table in ClickHouse'''
+    query = 'DROP TABLE IF EXISTS {table}'.format(
+        table=get_source_table_name(source))
+    get_clickhouse_data(query)
+
+
+def create_table(source, fields):
+    '''Creates table in ClickHouse for hits/visits with particular fields'''
+    tmpl = '''
+        CREATE TABLE {table_name} (
+            {fields}
+        ) ENGINE = {engine}
+    '''
+    field_tmpl = '{name} {type}'
+    field_statements = []
+
+    table_name = get_source_table_name(source)
+    if source == 'hits':
+        if ('ym:pv:date' in fields) and ('ym:pv:clientID' in fields):
+            engine = 'MergeTree() PARTITION BY toYYYYMM(Date) ORDER BY (ClientID, Date, intHash64(ClientID)) SAMPLE BY intHash64(ClientID) SETTINGS index_granularity=8192'
+        else:
+            engine = 'Log'
+
+    if source == 'visits':
+        if ('ym:s:date' in fields) and ('ym:s:clientID' in fields):
+            engine = 'MergeTree() PARTITION BY toYYYYMM(Date) ORDER BY (ClientID, Date, intHash64(ClientID)) SAMPLE BY intHash64(ClientID) SETTINGS index_granularity=8192'
+        else:
+            engine = 'Log'
+
+    ch_field_types = utils.get_ch_fields_config()
+    ch_fields = list(map(get_ch_field_name, fields))
+
+    for i in range(len(fields)):
+        field_statements.append(field_tmpl.format(name=ch_fields[i],
+                                                  type=ch_field_types[fields[i]]))
+
+    field_statements = sorted(field_statements)
+    query = tmpl.format(table_name=table_name,
+                        engine=engine,
+                        fields=',\n'.join(sorted(field_statements)))
+
+    get_clickhouse_data(query)
+
+
+def save_data(source, fields, data):
+    '''Inserts data into ClickHouse table'''
+
+    if not is_db_present():
+        logger.info('Database created')
+        create_db()
+
+    if not is_table_present(source):
+        logger.info('Table created')
+        create_table(source, fields)
+
+    upload(get_source_table_name(source), data)
+
+
+def is_data_present(start_date_str, end_date_str, source):
+    '''Returns whether there is a records in database for particular date range and source'''
+    if not is_db_present():
+        return False
+
+    if not is_table_present(source):
+        return False
+
+    table_name = get_source_table_name(source)
+    query = '''
+        SELECT count()
+        FROM {table}
+        WHERE Date >= '{start_date}' AND Date <= '{end_date}'
+    '''.format(table=table_name,
+               start_date=start_date_str,
+               end_date=end_date_str)
+
+    visits = get_clickhouse_data(query, CH_HOST)
+    is_null = (visits == '') or (visits.strip() == '0')
+    return not is_null
---
+++ logs_api.py
@@ -0,0 +1,199 @@
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
+import requests
+
+import json
+import utils
+import clickhouse
+import datetime
+import logging
+
+if utils.get_python_version().startswith('2'):
+    from urllib import urlencode
+else:
+    from urllib.parse import urlencode
+
+logger = logging.getLogger('logs_api')
+
+HOST = 'https://api-metrika.yandex.ru'
+
+
+def get_estimation(user_request):
+    '''Returns estimation of Logs API (whether it's possible to load data and max period in days)'''
+    url_params = urlencode(
+        [
+            ('date1', user_request.start_date_str),
+            ('date2', user_request.end_date_str),
+            ('source', user_request.source),
+            ('fields', ','.join(user_request.fields))
+        ]
+    )
+
+    headers = {'Authorization': 'OAuth ' + user_request.token}
+
+    url = '{host}/management/v1/counter/{counter_id}/logrequests/evaluate?' \
+              .format(host=HOST, counter_id=user_request.counter_id) + url_params
+
+    r = requests.get(url, headers=headers)
+
+    if r.status_code == 200:
+        return json.loads(r.text)['log_request_evaluation']
+    else:
+        raise ValueError(r)
+
+
+def get_api_requests(user_request):
+    '''Returns list of API requests for UserRequest'''
+    api_requests = []
+    estimation = get_estimation(user_request)
+    if estimation['possible']:
+        api_request = utils.Structure(
+            user_request=user_request,
+            date1_str=user_request.start_date_str,
+            date2_str=user_request.end_date_str,
+            status='new'
+        )
+        api_requests.append(api_request)
+    elif estimation['max_possible_day_quantity'] != 0:
+        start_date = datetime.datetime.strptime(
+            user_request.start_date_str,
+            utils.DATE_FORMAT
+        )
+
+        end_date = datetime.datetime.strptime(
+            user_request.end_date_str,
+            utils.DATE_FORMAT
+        )
+
+        days = (end_date - start_date).days
+        num_requests = int(days / estimation['max_possible_day_quantity']) + 1
+        days_in_period = int(days / num_requests) + 1
+        for i in range(num_requests):
+            date1 = start_date + datetime.timedelta(i * days_in_period)
+            date2 = min(
+                end_date,
+                start_date + datetime.timedelta((i + 1) * days_in_period - 1)
+            )
+
+            api_request = utils.Structure(
+                user_request=user_request,
+                date1_str=date1.strftime(utils.DATE_FORMAT),
+                date2_str=date2.strftime(utils.DATE_FORMAT),
+                status='new'
+            )
+            api_requests.append(api_request)
+    else:
+        raise RuntimeError('Logs API can\'t load data: max_possible_day_quantity = 0')
+    return api_requests
+
+
+def create_task(api_request):
+    '''Creates a Logs API task to generate data'''
+    url_params = urlencode(
+        [
+            ('date1', api_request.date1_str),
+            ('date2', api_request.date2_str),
+            ('source', api_request.user_request.source),
+            ('fields', ','.join(sorted(api_request.user_request.fields, key=lambda s: s.lower())))
+        ]
+    )
+
+    url = '{host}/management/v1/counter/{counter_id}/logrequests?' \
+              .format(host=HOST,
+                      counter_id=api_request.user_request.counter_id) \
+          + url_params
+
+    headers = {'Authorization': 'OAuth ' + api_request.user_request.token}
+
+    r = requests.post(url, headers=headers)
+    logger.debug(r.text)
+    if r.status_code == 200:
+        logger.debug(json.dumps(json.loads(r.text)['log_request'], indent=2))
+        response = json.loads(r.text)['log_request']
+        api_request.status = response['status']
+        api_request.request_id = response['request_id']
+        # api_request.size = response['size']
+        return response
+    else:
+        raise ValueError(r.text)
+
+
+def update_status(api_request):
+    '''Returns current tasks\'s status'''
+    url = '{host}/management/v1/counter/{counter_id}/logrequest/{request_id}' \
+        .format(request_id=api_request.request_id,
+                counter_id=api_request.user_request.counter_id,
+                host=HOST)
+
+    headers = {'Authorization': 'OAuth ' + api_request.user_request.token}
+
+    r = requests.get(url, headers=headers)
+    logger.debug(r.text)
+    if r.status_code == 200:
+        status = json.loads(r.text)['log_request']['status']
+        api_request.status = status
+        if status == 'processed':
+            size = len(json.loads(r.text)['log_request']['parts'])
+            api_request.size = size
+        return api_request
+    else:
+        raise ValueError(r.text)
+
+
+def save_data(api_request, part):
+    '''Loads data chunk from Logs API and saves to ClickHouse'''
+    url = '{host}/management/v1/counter/{counter_id}/logrequest/{request_id}/part/{part}/download' \
+        .format(
+        host=HOST,
+        counter_id=api_request.user_request.counter_id,
+        request_id=api_request.request_id,
+        part=part
+    )
+
+    headers = {'Authorization': 'OAuth ' + api_request.user_request.token}
+
+    r = requests.get(url, headers=headers)
+    if r.status_code != 200:
+        logger.debug(r.text)
+        raise ValueError(r.text)
+
+    splitted_text = r.text.split('\n')
+    logger.info('### DATA SAMPLE')
+    logger.info('\n'.join(splitted_text[:5]))
+
+    headers_num = len(splitted_text[0].split('\t'))
+    splitted_text_filtered = list(filter(lambda x: len(x.split('\t')) == headers_num, r.text.split('\n')))
+    num_filtered = len(splitted_text) - len(splitted_text_filtered)
+    if num_filtered != 0:
+        logger.warning('%d rows were filtered out' % num_filtered)
+
+    if len(splitted_text_filtered) > 1:
+        output_data = '\n'.join(splitted_text_filtered)  # .encode('utf-8')
+        output_data = output_data.replace(r"\'", "'")  # to correct escapes in params
+
+        clickhouse.save_data(api_request.user_request.source,
+                             api_request.user_request.fields,
+                             output_data)
+    else:
+        logger.warning('### No data to upload')
+
+    api_request.status = 'saved'
+
+
+def clean_data(api_request):
+    '''Cleans generated data on server'''
+    url = '{host}/management/v1/counter/{counter_id}/logrequest/{request_id}/clean' \
+        .format(host=HOST,
+                counter_id=api_request.user_request.counter_id,
+                request_id=api_request.request_id)
+
+    headers = {'Authorization': 'OAuth ' + api_request.user_request.token}
+
+    r = requests.post(url, headers=headers)
+    logger.debug(r.text)
+    if r.status_code != 200:
+        raise ValueError(r.text)
+
+    api_request.status = json.loads(r.text)['log_request']['status']
+    return json.loads(r.text)['log_request']
